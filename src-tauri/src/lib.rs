1b10a7d5e155f70bc28b78d6a6698c0f92f76b8d
Eloy <eloybercast@gmail.com>
2025-03-31 11:28:26 +0200

    Install the Tauri updater plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,12 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build());
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
