037e76618f45190e3232282ed34685021d6495bf
Eloy <eloybercast@gmail.com>
2025-04-04 17:58:02 +0200

    Add Auth page and update Tauri configuration for authentication window

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Updates from "./pages/Updates/Updates";
-
+import Auth from "./pages/Auth/Auth";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/updates" element={<Updates />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </Router>
   );
