9535597bcab08d6a186034071ba3be475f15339d
Eloy Bermejo <eloybercast@gmail.com>
2025-04-28 11:33:13 +0200

    Refactor FeaturedItem component: remove price display and adjust rarity section styling. Update styles for improved layout and responsiveness in FeaturedItems module.

--- src/pages/Lobby/components/Shop/components/ShopContent/components/FeaturedItems/components/FeaturedItem/FeaturedItem.jsx
+++ src/pages/Lobby/components/Shop/components/ShopContent/components/FeaturedItems/components/FeaturedItem/FeaturedItem.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import styles from "./FeaturedItem.module.scss";
-const FeaturedItem = ({ image, title, rarity, description, price }) => {
+const FeaturedItem = ({ image, title, rarity, description }) => {
   return (
     <main className={styles.featuredItem}>
       <section className={styles.featuredItem__image}>
         <img src={image} alt={title} />
       </section>
-      <section className={styles.featuredItem__rarity}>
-        <img src={rarity} alt="Rarity" />
-      </section>
       <section className={styles.featuredItem__content}>
         <h3 className={styles.featuredItem__title}>{title}</h3>
-        <h5 className={styles.featuredItem__price}>{price}</h5>
+        <div className={styles.featuredItem__rarity}>
+          <img src={rarity} alt="Rarity" />
+        </div>
       </section>
     </main>
   );
